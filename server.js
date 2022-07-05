466cb4796afe5d7978d6a6306fdcaa4fa28f4087
Marie Yee <marieyee02@yahoo.com>
2022-07-04 19:47:37 -0700

    modified code

--- server.js
+++ server.js
@@ -1,41 +1,199 @@
-const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
+//Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+//connection
+const connection = mysql.createConnection({
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL Username
-    user: 'root',
-    // TODO: Add MySQL Password
-    password: '',
-    database: 'books_db'
-  },
-  console.log(`Connected to the books_db database.`)
-);
-
-// Query database using COUNT() and GROUP BY
-db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
+  host: "localhost",
+  port: 3301,
+  user: "root",
+  password: "password10",
+  database: "employee_tracker"
 });
 
-// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
+connection.connect((err) => {
+  if (err) throw err;
+
+  runSearch()
 });
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+//code
+function runSEarch() {
+  inquirer.prompt({
+    name: "selection",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View all employees",
+      "View department",
+      "View role",
+      "Add employee",
+      "Add department",
+      "Add role",
+      "Update role",
+    
+    ]
+  })
+  .then(function(answer) {
+    console.log(answer);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    if (answer.selection === "View all employees") {
+      viewAll();
+    } else if(answer.selection === "View Department") {
+      viewDept();
+    } else if(answer.selection === "View role") {
+      viewRole();
+    } else if(answer.selection === "Add employee") {
+      addEmployee()
+    } else if (answer.selection === "Add department") {
+      addDept();
+    } else if(answer.selection === "Add role") {
+      addRole();
+    } else if(answer.selection === "Update role") {
+      updateRole();
+    } else {
+      connection.end();
+    }
+  });
+}
+
+//View all employees functions
+function viewAll() {
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id LEFT JOIN department ON role.department_id = department.id",
+    function(err, result, fields) {
+      if (err) throw err;
+      console.table(result);
+
+    runSearch();
+    }
+  );
+};
+
+function viewrole() {
+  connection.query(
+    "SELECT role.id, role.title, role.salary, role.department_id, department.id, department.name FROM role LEFT JOIN department on role.department_id = department.id",
+    function(err, result, fields) {
+      if (err) throw err;
+      console.table(result);
+
+      runSearch();
+    }
+  );
+};
+
+function viewDepts() {
+  connection.query("SELECT * FROM department,", function(err, results, fields) {
+    if (err) throw err;
+    console.table(result);
+    runSearch();
+  }
+  );
+};
+
+const roleChoices = [];
+const empChoices = [];
+const deptChoices = [];
+
+function lookuprole(){
+
+  connection.query("SELECT * FROM role", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      roleChoices.push(data[i] + "-" + data[i].title)
+    }
+  })
+}
+
+function lookupEmployee(){
+  connection.query("SELECT * FROM employee", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      empChoices.push(data[i].id + "-" + data[i].first_name+" "+ data[i].last_name)
+    }
+  })
+}
+
+function lookupDepts(){
+  connection.query("SELECT * FROM department", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      deptChoices.push(data[i].id + "-" + data[i].name)
+    }
+  })
+}
+
+function addEmployee9) {
+
+  lookuprole()
+  lookupEmployee()
+
+  inquirer.prompt([
+    {
+      name: "firstname",
+      type: "input",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "lastname",
+      type: "input",
+      message: "What is the employee's last name?",
+    },
+    {
+      name: "role",
+      type: "input",
+      message: "What is the employee's role?",
+      choices: roleChoices
+    },
+    {
+      name: "reportingTo",
+      type: "list",
+      message: "Who is the employee's manager?",
+      choices: empChoices
+    },
+  ]).then(function(answer) {
+    const getRoleId = answer.role.split("-")
+    const getReportingToId = answer.reportingTo.split("-")
+    const query=
+    `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES ('${answer.firstname}', '${answer.lastname}', '${getRoleId[0]}', '${getReportingToId[0]}')'`;
+    connection.query(query, function(err, res) {
+      console.log(`new employee ${answer.firstname} ${answer.lastname} added.`)
+    });
+    runSearch();
+  });
+};
+
+function addRole() {
+
+  lookuprole()
+  lookupEmployee()
+  lookupDepts()
+
+  inquirer.prompt([
+    {
+      name: "role",
+      type: "input",
+      message: "Enter the role you would like to add:"
+    },
+    {
+      name: "dept",
+      type: "list",
+      message: "Which department do you want to add this role to?",
+      choices: deptChoices
+    },
+    {
+      name: "salary",
+      type: "number",
+      message: "Enter the role's salary."
+    },
+  ]).then(function(answer) {
+    console.log(`${answer.role}`)
+    const getDeptId = answer.dept.split('-')
+    const query = 
+    `INSERT INTO role (title, salary, department_id)
+    VALUES ('${answer.role}', '${answer.salary}', '${getDeptId[0]}')`;
+  })
+}
